169befec5f243b1b66fae752822c24643774d181
Leandro <leandro.vieira.silva@usp.br>
2020-04-22 09:24:25 -0300

    Atualização - curso aws

---
+++ Projetos de Estudos/Lives Mario Filho/Deploy/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import joblib as jb
+import json
+
+app = Flask(__name__)
+
+mdl = jb.load("mdl.pkl.z")
+
+@app.route("/")
+def main():
+    
+    print(request.args)
+
+    title = request.args.get("titulo", default=' ')
+    res = {"titulo":title, "p":mdl.predict_proba([title])[0][1]}
+    return json.dumps(res)
+
+if __name__ == "__main__":
+    app.run()
+
+
